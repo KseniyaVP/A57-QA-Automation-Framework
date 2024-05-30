0960780ebd9ec3f9aad12ecf471d653cb61ddaaa
KseniyaVP <164698826+KseniyaVP@users.noreply.github.com>
2024-05-29 22:07:35 -0400

    homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,17 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
 
-    public WebDriver driver;
+    public static WebDriver driver;
 
     public WebDriverWait wait;
 
@@ -38,10 +42,8 @@ public class BaseTest {
     }*/
 @BeforeMethod
 @Parameters({"BaseURL"})
-public void launchBrowser(String baseURL) {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--remote-allow-origins=*");
-    driver = new ChromeDriver(options);
+public void launchBrowser(String baseURL) throws MalformedURLException {
+    driver = pickBrowser(System.getProperty("browser"));
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.manage().window().maximize();
     wait =new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -50,6 +52,25 @@ public void launchBrowser(String baseURL) {
 @AfterMethod
     public void closeBrowser(){
     driver.quit();
+    }
+
+    public static WebDriver pickBrowser (String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.0.17:4444";
+        switch (browser){
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(chromeOptions);
+        }
     }
 
     public void clickLoginBtn() {
